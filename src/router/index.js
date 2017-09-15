1bed9ab651a850d7bc4c0ffdfbfa51ac32632961
sunchang <sunchang612@gmail.com>
2017-09-15 14:18:58 +0800

    [ADD] 搭建简单结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-
+import Catelog from '@/views/Catalog'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Catelog',
+      component: Catelog
     }
   ]
 })
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,14 +1,12 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div class="wrapper">
   </div>
 </template>
-
 <script>
 export default {
-  name: 'app'
 }
 </script>
+<style lang="less" scoped>
 
-<style>
 </style>
+
